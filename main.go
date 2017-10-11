a19f11e1c49dab86f43263f465951d5cb77218e9
王涛 <wangtao101rs@163.com>
2017-10-11 09:45:11 +0800

    Version cron_prefix

--- main.go
+++ main.go
@@ -31,14 +31,16 @@ func main() {
 	beego.AppConfig.Set("version", VERSION)
 
 	// 路由设置
-	beego.Router("/", &controllers.MainController{}, "*:Index")
-	beego.Router("/login", &controllers.MainController{}, "*:Login")
-	beego.Router("/logout", &controllers.MainController{}, "*:Logout")
-	beego.Router("/profile", &controllers.MainController{}, "*:Profile")
-	beego.Router("/gettime", &controllers.MainController{}, "*:GetTime")
-	beego.Router("/help", &controllers.HelpController{}, "*:Index")
-	beego.AutoRouter(&controllers.TaskController{})
-	beego.AutoRouter(&controllers.GroupController{})
+	beego.Router("/cron", &controllers.MainController{}, "*:Index")
+	beego.Router("/cron/login", &controllers.MainController{}, "*:Login")
+	beego.Router("/cron/logout", &controllers.MainController{}, "*:Logout")
+	beego.Router("/cron/profile", &controllers.MainController{}, "*:Profile")
+	beego.Router("/cron/gettime", &controllers.MainController{}, "*:GetTime")
+	beego.Router("/cron/help", &controllers.HelpController{}, "*:Index")
+	//beego.AutoRouter(&controllers.TaskController{})
+	//beego.AutoRouter(&controllers.GroupController{})
+	beego.AutoPrefix("/cron/", &controllers.TaskController{})
+	beego.AutoPrefix("/cron/", &controllers.GroupController{})
 
 	beego.BConfig.WebConfig.Session.SessionOn = true
 	beego.Run()
